2a747fb551e6ae46b6279ca233284d361e3d69b8
Abimael Mendes <abimaelmends@hotmail.com>
2024-08-08 17:39:01 -0300

    Add project files.

---
+++ CacheDemo/Controllers/ProductsController.cs
@@ -0,0 +1,35 @@
+﻿using CacheDemo.Data;
+using CacheDemo.Models;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Caching.Memory;
+
+[ApiController]
+[Route("[controller]")]
+public class ProductsController : ControllerBase
+{
+    private readonly IMemoryCache _memoryCache;
+
+    public ProductsController(IMemoryCache memoryCache)
+    {
+        _memoryCache = memoryCache;
+    }
+
+    [HttpGet]
+    public IActionResult Get()
+    {
+        const string cacheKey = "products";
+
+        if (!_memoryCache.TryGetValue(cacheKey, out List<Product> products))
+        {
+            products = AppData.Estoque();
+
+            var cacheEntryOptions = new MemoryCacheEntryOptions
+            {
+                AbsoluteExpirationRelativeToNow = TimeSpan.FromMinutes(5)
+            };
+            _memoryCache.Set(cacheKey, products, cacheEntryOptions);
+        }
+
+        return Ok(products);
+    }
+}
---
+++ CacheDemo/Data/AppData.cs
@@ -0,0 +1,25 @@
+﻿using CacheDemo.Models;
+
+namespace CacheDemo.Data
+{
+    public static class AppData
+    {
+        public static List<Product> Estoque()
+        {
+
+            List<Product> Products = new List<Product>
+            {
+                new Product { Id = 1, Name = "Geladeira", Price = 1899.99m },
+                new Product { Id = 2, Name = "Ventilador", Price = 399.99m },
+                new Product { Id = 3, Name = "Cama", Price = 1299.99m },
+                new Product { Id = 4, Name = "Micro-ondas", Price = 599.99m },
+                new Product { Id = 5, Name = "Fogão", Price = 899.99m },
+                new Product { Id = 6, Name = "Televisão LED 4K", Price = 2399.99m },
+                new Product { Id = 7, Name = "Aspirador de Pó", Price = 249.99m },
+                new Product { Id = 8, Name = "Máquina de Lavar", Price = 1499.99m }
+            };
+
+            return Products;
+        }
+    }
+}
